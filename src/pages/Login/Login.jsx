8e5100853b3240cf327f90f6fcf7ef7322916953
Maryna Karamanova <karamanova91@gmail.com>
2023-03-31 04:04:57 +0300

    updated LogIn

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,18 @@
 import { MainTitle } from "components/common/MainTitle.styled";
 import { LoginForm } from "components/auth/LoginForm/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLoginSuccess = () => {
+    navigate("/contacts");
+  };
+
   return (
     <>
       <MainTitle>If you already have an account, please log in</MainTitle>
-      <LoginForm />
+      <LoginForm onSuccess={handleLoginSuccess} />
     </>
   );
 };
